b00e35716cd5b015a30276f8c24eeecb99838139
Vanessa Y <vyeung.wt@gmail.com>
2021-03-04 22:58:35 +0000

    add reservation query and sanitize

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -86,7 +86,22 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  console.log('guest id', guest_id)
+  return pool.query(`
+  SELECT properties.*, start_date, avg(property_reviews.rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1 
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `, [guest_id, limit])
+  .then((data) => {
+    console.log(data.rows);
+    return data.rows;
+  })
+  .catch(() => err(null));
 }
 exports.getAllReservations = getAllReservations;
 
